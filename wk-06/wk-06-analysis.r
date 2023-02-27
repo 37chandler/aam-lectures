c9cd6d15bee9d2d431e3193ba97dcfadf9c16e71
John Chandler <john.chandler@gmail.com>
2023-02-27 17:22:00 -0600

    pushed wk6

---
+++ wk-06/wk-06-analysis.r
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(here)
+
+d <- read_tsv(here("wk-06","fake_traffic_accident_data.txt"))
+
+lm_1 <- lm(accidents_per_10K_1 ~ weekday + weather + 
+             temperature + pedestrian_level,data=d)
+
+summary(lm_1)
